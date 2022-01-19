3b5f016a95fc429e7deaf69fb44565b4b4324f5d
harel sela <harel10003@gmail.com>
2022-01-19 10:28:49 +0200

    chenges

--- Expading Cards(1)/script.js
+++ Expading Cards(1)/script.js
@@ -1,4 +1,4 @@
-const panels = document.querySelectorAll('.panel')
+const panels = document.querySelectorAll(".panel");
 
 // panels.forEach((panel) => {
 //   panel.addEventListener('click', () => {
@@ -7,51 +7,46 @@ const panels = document.querySelectorAll('.panel')
 //   })
 // })
 
-function nextPage() {
-  panels.forEach((panel, index) => {
-    panel.addEventListener('click', () => {
-      if (index + 1 < panels.length) {
-        remoeActiveClasses()
-        panels[index + 1].classList.add('active')
-        console.log(index + 2, panels.length)
-      }
-    })
-  })
+function nextPage(num) {
+  remoeActiveClasses();
+  panels[num].classList.add("active");
 }
 
 function remoeActiveClasses() {
   panels.forEach((panel) => {
-    panel.classList.remove('active')
-  })
+    panel.classList.remove("active");
+  });
 }
-function changeLevel() {
-  panels.forEach((panel, index) => {
-    panel.addEventListener('click', () => {
-      remoeActiveClasses()
-      panels[index - 1].classList.add('active')
-    })
-  })
+function changeLevel(num) {
+  remoeActiveClasses();
+  nextPage(num);
 }
-function exit() {
-  panels.forEach((panel, index) => {
-    panel.addEventListener('click', () => {
-      remoeActiveClasses()
-      panels[index - 2].classList.add('active')
-    })
-  })
+function exit(num) {
+  remoeActiveClasses();
+  document.querySelector("#userName").value = "";
+  document.querySelector("#password").value = "";
+  nextPage(num);
 }
-function start() {
-  let name = document.querySelector('#userName')
-  let pass = document.querySelector('#password')
-  if (name.value === 'abcd' && pass.value === '1234') {
-    nextPage()
+
+function start(id) {
+  let name = document.querySelector("#userName");
+  let pass = document.querySelector("#password");
+  if (name.value !== "abcd" && pass.value !== "1234") {
+    document.querySelector("#msgUName").innerHTML = "this input not corect";
+    document.querySelector("#msgPass").innerHTML = "this input not corect";
+  } else if (name.value !== "abcd")
+    document.querySelector("#msgUName").innerHTML = "this input not corect";
+  else if (pass.value !== "1234")
+    document.querySelector("#msgPass").innerHTML = "this input not corect";
+  else if (name.value === "abcd" && pass.value === "1234") {
+    nextPage(id);
   }
 }
 
-function newGame(level) {
-  let a
-  let b = 10
-  let randShow
+function newGame(level, num) {
+  let a;
+  let b = 10;
+  let randShow;
 
   let mat = [
     [1, 8, 5, 3, 4, 9, 6, 7, 2],
@@ -63,30 +58,30 @@ function newGame(level) {
     [2, 3, 8, 9, 6, 1, 5, 4, 7],
     [4, 6, 7, 5, 2, 8, 3, 9, 1],
     [9, 5, 1, 4, 3, 7, 8, 2, 6],
-  ]
+  ];
   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
-      b++
-      a = '#a' + b
-      document.querySelector(a).value = ''
+      b++;
+      a = "#a" + b;
+      document.querySelector(a).value = "";
     }
   }
-  b = 10
+  b = 10;
   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
-      b++
-      a = '#a' + b
-      randShow = Math.floor(Math.random() * 10)
+      b++;
+      a = "#a" + b;
+      randShow = Math.floor(Math.random() * 10);
       if (randShow > level) {
-        document.querySelector(a).value = mat[i][j]
+        document.querySelector(a).value = mat[i][j];
       }
     }
   }
-  nextPage()
+  nextPage(num);
 }
 function checkGame() {
-  let x
-  b = 10
+  let x;
+  b = 10;
   let mat = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -97,63 +92,70 @@ function checkGame() {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  ]
+  ];
   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
-      b++
-      a = '#a' + b
+      b++;
+      a = "#a" + b;
 
-      x = document.querySelector(a)
-      mat[i][j] = x.value
+      x = document.querySelector(a);
+      mat[i][j] = x.value;
     }
   }
-  console.log(mat)
-  let column = 0
-  let Row = 0
-  let index
-  let num
-  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  console.log(mat);
+  let column = 0;
+  let Row = 0;
+  let index;
+  let num;
+  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
-      num = +mat[i][j]
-      index = arr.indexOf(num)
+      num = +mat[i][j];
+      index = arr.indexOf(num);
       if (index === -1)
-        return console.log('not good column mat[', i, '][', j, ']')
-      else arr[index] = 0
+        return console.log("not good column mat[", i, "][", j, "]");
+      else arr[index] = 0;
     }
-    arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   }
 
   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 9; j++) {
-      num = +mat[j][i]
-      index = arr.indexOf(num)
-      if (index === -1) return console.log('not good row mat[', i, '][', j, ']')
-      else arr[index] = 0
+      num = +mat[j][i];
+      index = arr.indexOf(num);
+      if (index === -1)
+        return console.log("not good row mat[", i, "][", j, "]");
+      else arr[index] = 0;
     }
-    arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   }
   let col = 0,
-    ro = 0
-  arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    ro = 0;
+  arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   for (let z = 0; z < 9; z++) {
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
-        num = +mat[i + col][j + ro]
-        index = arr.indexOf(num)
+        num = +mat[i + col][j + ro];
+        index = arr.indexOf(num);
         if (index === -1)
-          return console.log('not good Square mat[', i + col, '][', j + ro, ']')
-        else arr[index] = 0
+          return console.log(
+            "not good Square mat[",
+            i + col,
+            "][",
+            j + ro,
+            "]"
+          );
+        else arr[index] = 0;
       }
     }
 
-    ;(i = 0), (j = 0), (arr = [1, 2, 3, 4, 5, 6, 7, 8, 9])
+    (i = 0), (j = 0), (arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]);
     if (ro === 6) {
-      col = col + 3
-      ro = 0
-    } else ro = ro + 3
+      col = col + 3;
+      ro = 0;
+    } else ro = ro + 3;
   }
 
-  return alert('kol hakavod yagever')
+  return alert("kol hakavod yagever");
 }
